bfc46921ce555b5a921abe8dd8db9bac0c48da8b
ahmad.kayali <ahmad.kayali@sultan-center.com>
2019-08-11 22:20:40 +0300

    add app components

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import logo from '../logo.svg';
+
+const Header = () => (
+  <header>
+      <img width={100} src={logo} alt="logo" />
+      <h1>React Restaurants Reservation</h1>
+  </header>
+);
+
+export default Header;
