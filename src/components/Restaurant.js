60eab2143e4ed2f414dcac8f45b547b14bf16ca3
ahmad.kayali <ahmad.kayali@sultan-center.com>
2019-08-26 15:36:15 +0300

    working on datePicker functionality

--- src/components/Restaurant.js
+++ src/components/Restaurant.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Gallery from '../gallery';
 import style from '../style/Restaurant.module.css';
+import DatePicker from 'react-datepicker';
+
+import 'react-datepicker/dist/react-datepicker.css';
 
 const Restaurant = ({info}) => {
+    const minDate = new Date();
+
+    const [portalState, setPortalState] = useState(false);
+    const [date, setDate]               = useState(minDate.setDate(minDate.getDate()+1));
+
+    const handleChange = (date) => {
+        setDate(date);
+        toggleCalendar();
+    };
+
+    const toggleCalendar = () => {
+        setPortalState(!portalState);
+    };
+
     return (
-        <div className={[style.item, style.box_shadow].join(' ')}>
+        <div className={[style.item, style.box_shadow].join(' ')} onClick={toggleCalendar}>
             <h2>{info.name}</h2>
             <img src={Gallery[info.id]} alt={info.name} />
             <div className={style.item_info}>
@@ -22,6 +39,18 @@ const Restaurant = ({info}) => {
             <div>
                 <p>Cuisines:<br/> {info.cuisines.join('-')}</p>
             </div>
+            {
+                portalState && (
+                    <DatePicker
+                        selected={date}
+                        onChange={handleChange}
+                        minDate={minDate}
+                        onClickOutside={toggleCalendar}
+                        withPortal
+                        inline
+                    />
+                )
+            }
         </div>
     )
 };
