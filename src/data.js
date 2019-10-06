a69e8cac346b3b4c0db9ca6f30a02582f15b5948
ahmad.kayali <ahmad.kayali@sultan-center.com>
2019-10-06 14:15:11 +0300

    add new data set

--- src/data.js
+++ src/data.js
@@ -1,63 +1,119 @@
 const restaurantsData = [
     {
-        id: 'burgerKing',
-        name: 'Burger King',
-        price: 1,
-        rating: 2,
-        cuisines: ['burgers', 'fast food']
+        id: 'res_1',
+        name: 'Chai Thali',
+        price: 2,
+        rating: 3,
+        cuisines: ['indian', 'chicken']
     },
     {
-        id: 'kfc',
-        name: 'KFC',
-        price: 2,
+        id: 'res_2',
+        name: 'Burger King',
+        price: 1,
         rating: 2,
         cuisines: ['burgers', 'fast food', 'chicken']
     },
     {
-        id: 'alia',
-        name: 'Alia',
+        id: 'res_3',
+        name: 'Noodle',
         price: 3,
-        rating: 3,
-        cuisines: ['arabic', 'breakfast', 'chicken', 'seafood', 'grills']
+        rating: 4,
+        cuisines: ['chinese', 'breakfast', 'chicken', 'seafood']
     },
     {
-        id: 'ward',
-        name: 'Ward',
-        price: 5,
+        id: 'res_4',
+        name: 'Charcoal',
+        price: 4,
         rating: 5,
         cuisines: ['arabic', 'breakfast', 'chicken', 'grills']
     },
     {
-        id: 'rakoon',
-        name: 'Rakoon',
+        id: 'res_5',
+        name: 'Red Herring',
         price: 4,
         rating: 3,
-        cuisines: ['indian', 'chicken', 'grills']
+        cuisines: ['seafood']
     },
     {
-        id: 'casereccio',
-        name: 'Casereccio',
-        price: 4,
-        rating: 3,
+        id: 'res_6',
+        name: 'Zssence',
+        price: 3,
+        rating: 2,
         cuisines: ['italian', 'pasta', 'pizza']
     },
     {
-        id: 'wox',
-        name: 'Wox',
+        id: 'res_7',
+        name: 'Garramone',
         price: 3,
         rating: 4,
-        cuisines: ['indian', 'chinese']
+        cuisines: ['italian', 'pasta', 'pizza']
     },
     {
-        id: 'kababji',
-        name: 'Kababji',
+        id: 'res_8',
+        name: 'Kalbo',
+        price: 2,
+        rating: 4,
+        cuisines: ['steaks', 'grills', 'chicken']
+    },
+    {
+        id: 'res_9',
+        name: 'Plant & Bean',
         price: 4,
         rating: 4,
-        cuisines: ['arabic', 'grills', 'chicken']
+        cuisines: ['pasta', 'pizza']
+    },
+    {
+        id: 'res_10',
+        name: 'Pizza Hut',
+        price: 2,
+        rating: 3,
+        cuisines: ['pizza', 'fast food']
+    },
+    {
+        id: 'res_11',
+        name: 'Local Pie',
+        price: 4,
+        rating: 5,
+        cuisines: ['breakfast', 'chicken']
+    },
+    {
+        id: 'res_12',
+        name: 'Wooden Spoon',
+        price: 5,
+        rating: 5,
+        cuisines: ['grills', 'steaks']
+    },
+    {
+        id: 'res_13',
+        name: 'Burrito',
+        price: 4,
+        rating: 4,
+        cuisines: ['mexican', 'grills', 'chicken']
+    },
+    {
+        id: 'res_14',
+        name: 'Steak House',
+        price: 5,
+        rating: 5,
+        cuisines: ['steaks', 'grills']
+    },
+    {
+        id: 'res_15',
+        name: 'Half Wall',
+        price: 3,
+        rating: 2,
+        cuisines: ['burgers', 'grills', 'chicken']
+    },
+    {
+        id: 'res_16',
+        name: 'Nevada',
+        price: 5,
+        rating: 5,
+        cuisines: ['mexican', 'grills', 'chicken']
     },
 
 ];
 
-const cuisinesData = ['arabic', 'chicken', 'breakfast', 'burgers', 'indian', 'italian', 'seafood', 'chinese', 'pasta', 'steaks', 'grills', 'pizza'];
+const cuisinesData = ['arabic', 'chicken', 'breakfast', 'burgers', 'indian', 'italian', 'seafood', 'chinese', 'pasta', 'steaks', 'grills', 'pizza', 'mexican'];
 
 export { restaurantsData , cuisinesData};
--- src/gallery.js
+++ src/gallery.js
@@ -1,19 +1,35 @@
-import rakoon from './images/rakoon.png';
-import burgerKing from './images/burger-king.jpg';
-import kfc from './images/kfc.jpg';
-import alia from './images/alia.jpg';
-import ward from './images/ward.jpg';
-import casereccio from './images/casereccio.jpg';
-import kababji from './images/kababji.jpg';
-import wox from './images/wox.jpg';
+import res_1 from './images/res_logo_1.png';
+import res_2 from './images/res_logo_2.png';
+import res_3 from './images/res_logo_3.png';
+import res_4 from './images/res_logo_4.png';
+import res_5 from './images/res_logo_5.png';
+import res_6 from './images/res_logo_6.png';
+import res_7 from './images/res_logo_7.png';
+import res_8 from './images/res_logo_8.png';
+import res_9 from './images/res_logo_9.png';
+import res_10 from './images/res_logo_10.png';
+import res_11 from './images/res_logo_11.png';
+import res_12 from './images/res_logo_12.png';
+import res_13 from './images/res_logo_13.png';
+import res_14 from './images/res_logo_14.png';
+import res_15 from './images/res_logo_15.png';
+import res_16 from './images/res_logo_16.png';
 
 export default {
-    rakoon,
-    burgerKing,
-    kfc,
-    alia,
-    ward,
-    casereccio,
-    kababji,
-    wox,
+    res_1,
+    res_2,
+    res_3,
+    res_4,
+    res_5,
+    res_6,
+    res_7,
+    res_8,
+    res_9,
+    res_10,
+    res_11,
+    res_12,
+    res_13,
+    res_14,
+    res_15,
+    res_16,
 }
